3e2f679c764372972aa4a8be5ceaa3247be5faaa
Jordan Laramie <jordanlaramie@gmail.com>
2023-04-15 13:13:00 -0400

    Updated healthcheck to call and return the number of guilds the bot is in

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,6 +1,35 @@
-import { REST } from 'discord.js';
+import {
+  REST,
+  Routes,
+  type RESTGetAPICurrentUserGuildsResult
+} from 'discord.js';
 
 // Construct and prepare an instance of the REST module
 export const rest = new REST({ version: '10' }).setToken(
   process.env.BOT_TOKEN!
 );
+
+export async function getAllGuilds() {
+  const guilds: RESTGetAPICurrentUserGuildsResult = [];
+
+  let lastGuildId: string | undefined;
+  do {
+    const query = new URLSearchParams({ limit: '200' });
+    if (lastGuildId) {
+      query.append('after', lastGuildId);
+    }
+
+    const pagedGuilds = (await rest.get(Routes.userGuilds(), {
+      query
+    })) as RESTGetAPICurrentUserGuildsResult;
+
+    guilds.push(...pagedGuilds);
+
+    lastGuildId =
+      pagedGuilds.length === 200
+        ? pagedGuilds[pagedGuilds.length - 1].id
+        : undefined;
+  } while (lastGuildId);
+
+  return guilds;
+}
